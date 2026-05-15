3df1bebfc3c8e7b73a3b7870dd981e9f5b5aedae
Mayhesa Rachmadiharja <mayhesa.r@gmail.com>
2026-05-15 17:35:48 +0700

    Commit 9

--- UMKM/app/Providers/AppServiceProvider.php
+++ UMKM/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use OpenAI;
 
 class AppServiceProvider extends ServiceProvider
@@ -39,6 +40,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
